471e2e86c801885e96693eb08042d60d98980ee7
stiig <vasiliyrps2@gmail.com>
2016-07-04 15:49:16 +0500

    add dict for settings

--- scrap.py
+++ scrap.py
@@ -17,38 +17,45 @@ import json
 # with open('config.json', 'w') as f:
 #     json.dump(config, f)
 
-ext_file = 'txt'
+settings = dict(
+    ext_file='txt',
 
-header_elem = 'h1'
-header_attrs = {'class': 'b-topic__title'}
+    header_elem='h1',
+    header_attrs={'class': 'b-topic__title'},
 
-article_elem = 'div'
-article_attrs = {'itemprop': 'articleBody'}
-article_indent = '\n\n'
+    article_elem='div',
+    article_attrs={'itemprop': 'articleBody'},
+    article_indent='\n\n',
 
-sub_article_elem = 'p'
+    sub_article_elem='p',
+    text_width=80,
+)
 
-wrapper = TextWrapper(width=80)
+wrapper = TextWrapper(width=settings['text_width'])
 
 
-def main():
-    response = requests.get('https://lenta.ru/news/2016/07/01/vapersong/')
-    parsed_html = BeautifulSoup(response.text, "html.parser")
-    text = ''
-    header = wrapper.fill(parsed_html.find(header_elem, header_attrs).get_text())
-    text += header
-    wrapper.initial_indent = article_indent
-    for paragraph in parsed_html.find(article_elem, article_attrs).find_all(sub_article_elem):
-        for link in paragraph.find_all('a'):
-            link.replace_with('{}[{}]'.format(link.get_text(), link['href']))
-        text += wrapper.fill(paragraph.text.strip())
+class MainProgram:
+    @staticmethod
+    def run():
+        response = requests.get('https://lenta.ru/news/2016/07/01/vapersong/')
+        parsed_html = BeautifulSoup(response.text, "html.parser")
+        text = ''
+        header = wrapper.fill(parsed_html.find(settings['header_elem'], settings['header_attrs']).get_text())
+        text += header
+        wrapper.initial_indent = settings['article_indent']
+        for paragraph in parsed_html \
+                .find(settings['article_elem'], settings['article_attrs']) \
+                .find_all(settings['sub_article_elem']):
+            for link in paragraph.find_all('a'):
+                link.replace_with('{}[{}]'.format(link.get_text(), link['href']))
+            text += wrapper.fill(paragraph.text.strip())
 
-    final_url = urlparse(response.url)
-    filename = '{}{}.{}'.format(final_url.netloc, final_url.path[:-1], ext_file)
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, "w") as f:
-        f.write(text)
+        final_url = urlparse(response.url)
+        filename = '{}{}.{}'.format(final_url.netloc, final_url.path[:-1], settings['ext_file'])
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as f:
+            f.write(text)
 
 
 if __name__ == "__main__":
-    main()
+    MainProgram.run()
