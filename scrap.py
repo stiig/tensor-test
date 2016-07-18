5adb9e7459bb77728e08b62c4d1b0e39902ef4d8
stiig <vasiliyrps2@gmail.com>
2016-07-19 04:21:29 +0500

    replace third party libraries on std lib. Also add intelligent algorithm for search article. Removed settings for special hosts

--- scrap.py
+++ scrap.py
@@ -1,11 +1,15 @@
 import argparse
 import json
+import urllib.request
+import urllib.error
+
+import operator
+from html.parser import HTMLParser
 from textwrap import TextWrapper
 from urllib.parse import urlparse
 
 import os
-import requests
-from bs4 import BeautifulSoup
+import re
 
 
 class Settings:
@@ -31,39 +35,108 @@ class Settings:
         pass
 
 
+class HTMLSourceParser(HTMLParser):
+    """
+    Здесь конроллируется вложенность тегов
+    """
+    def __init__(self, *, convert_charrefs=True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.header_recording = 0
+        self.div_recording = 0
+        self.header_data = []
+        self.div_data = {}
+
+    def error(self, message):
+        pass
+
+    def handle_starttag(self, tag, attrs):
+        if tag != 'h1' and tag != 'div' and tag != 'a':
+            return
+
+        if self.header_recording and tag == 'h1':
+            self.header_recording += 1
+            return
+        if self.div_recording and tag == 'div':
+            self.div_recording += 1
+            return
+
+        if tag == 'h1':
+            self.header_recording = 1
+        if tag == 'div':
+            self.div_recording = 1
+
+    def handle_endtag(self, tag):
+        if tag == 'h1' and self.header_recording:
+            self.header_recording -= 1
+        if tag == 'div' and self.div_recording:
+            self.div_recording -= 1
+
+    def handle_data(self, data):
+        if self.header_recording:
+            self.header_data.append(data.strip())
+        if self.div_recording and data.strip():
+            inner_data = self.div_data.get(self.div_recording, [])
+            inner_data.append(data.strip())
+            self.div_data[self.div_recording] = inner_data
+
+
 class Parser:
     """
     Основной разбор происходит в этом классе, при создании экземпляра класса
-    так же задаются настройки, как глобальные так и для обрабатываемого хоста.
-    По алгоритму каждая статья должна иметь тег заголовка, для более точного
-    определения так же указывается класс или иной атрибут заголовка, затем
-    происходит разбор содержания статьи, опять же как правило статья находится
-    в окружающем теге с определенным классом, мы задаем этот элемент в настройках,
-    а так же задаем атрибут, чтобы можно было точнее определить элемент и
-    пробегаем по дочерним элементам, параграфам статьи, собираем в одну переменную
-    и отдаем при вызове функции.
+    так же задаются глобальные настройки. При помощи внутреннего парсера
+    разбираем страницу, находим див который содержит больше всего дивов в себе
+    и отдаем его как контент статьи.
     Для работы обязательно нужно задать настройки для домена
     """
-    def __init__(self, raw_data, settings, host_settings):
-        self.page = BeautifulSoup(raw_data, "html.parser")
+
+    def __init__(self, raw_data, settings):
+        self.page = raw_data
         self.settings = settings
-        self.host_settings = host_settings
         self.wrapper = TextWrapper(width=self.settings.get('text_width'))
+        self.__parsed_title = None
+        self.__parsed_content = None
+        self.prepare()
+
+    def prepare(self):
+        removed_tags = [
+            'script',
+            'aside',
+            'header',
+            'style',
+            'nav',
+            'section',
+            'footer',
+            'noindex',
+        ]
+
+        result = self.page
+        for tag in removed_tags:
+            rx = "<{0}[\s\S]+?/{0}>".format(tag)
+            pattern = re.compile(rx)
+            result = re.sub(pattern, '', result)
+
+        pattern_link = re.compile(r'<a\s+[^>]*?href="([^"]*)".*?>(.*?)<\/a>', re.DOTALL)
+        result = pattern_link.sub(r'\2[\1]', result)
+
+        inner_parser = HTMLSourceParser()
+        inner_parser.feed(result)
+        inner_parser.close()
+
+        divs = {k: len(v) for k, v in inner_parser.div_data.items()}
+        max_divs = max(divs.items(), key=operator.itemgetter(1))[0]
+
+        self.__parsed_title = inner_parser.header_data[0]
+        self.__parsed_content = inner_parser.div_data[max_divs]
 
     def get_title(self):
         self.wrapper.initial_indent = ""
-        return self.wrapper.fill(self.page.find(self.host_settings['header_elem'],
-                                                self.host_settings['header_attrs']).get_text())
+        return self.wrapper.fill(self.__parsed_title)
 
     def get_article_body(self):
         text = ''
         self.wrapper.initial_indent = self.settings.get('article_indent')
-        for paragraph in self.page \
-                .find(self.host_settings['article_elem'], self.host_settings['article_attrs']) \
-                .find_all(self.host_settings['sub_article_elem']):
-            for link in paragraph.find_all('a'):
-                link.replace_with('{}[{}]'.format(link.get_text(), link['href']))
-            text += self.wrapper.fill(paragraph.text.strip())
+        for paragraph in self.__parsed_content:
+            text += self.wrapper.fill(paragraph)
 
         return text
 
@@ -92,12 +165,16 @@ class MainProgram:
     def run(url):
         settings = Settings()
         article = Article(url)
-        host_settings = settings.get(article.url.netloc)
-        if not host_settings:
-            raise TypeError('Settings for current host not found')
 
-        response = requests.get(url)
-        parser = Parser(response.text, settings, host_settings)
+        response = ''
+        try:
+            with urllib.request.urlopen(url) as f:
+                response = f.read().decode(f.headers.get_content_charset())
+        except urllib.error.URLError as e:
+            print('Something went wrong: ', e)
+            exit()
+
+        parser = Parser(response, settings)
 
         article.set_title(parser.get_title())
         article.set_content(parser.get_article_body())
